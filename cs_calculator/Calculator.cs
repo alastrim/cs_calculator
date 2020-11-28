329aee47514db6a79ae250446163d976bcf07a6c
alastrim <nick.alastrim@gmail.com>
2020-11-28 17:17:13 +0300

    Code done, time to figure out why it doesnt work

---
+++ cs_calculator/Calculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cs_calculator
+{
+    class Calculator
+    {
+        Queue<Token> m_input_queue;
+        Stack<Token> m_memory_stack;
+
+        Calculator (Queue<Token> input_queue)
+        {
+            m_input_queue = input_queue;
+            m_memory_stack = new Stack<Token> ();
+        }
+
+        void Calculate ()
+        {
+            foreach (Token token in m_input_queue)
+            {
+                if (token is TokenNumber)
+                {
+                    m_memory_stack.Push (token);
+                    continue;
+                }
+
+                if (token is TokenOperator @toperator)
+                {
+                    double a = ((TokenNumber)m_memory_stack.Pop ()).Value;
+                    double b = ((TokenNumber)m_memory_stack.Pop ()).Value;
+                    double c = 0;
+
+                    switch (toperator.Type)
+                    {
+                        case OperatorType.Add:
+                            c = a + b; break;
+                        case OperatorType.Subtract:
+                            c = a - b; break;
+                        case OperatorType.Multiply:
+                            c = a * b; break;
+                        case OperatorType.Divide:
+                            c = a / b; break;
+                        default:
+                            Console.WriteLine ("Something went horribly wrong!");
+                            break;
+                    }
+
+                    m_memory_stack.Push (new TokenNumber (c));
+                }
+            }
+        }
+
+        public static double CalculateAndDie (Queue<Token> input)
+        {
+            Calculator calc = new Calculator (input);
+            calc.Calculate ();
+            if (calc.m_memory_stack.Count > 1)
+                Console.WriteLine ("Something went horribly wrong!");
+            return ((TokenNumber)calc.m_memory_stack.Pop ()).Value;
+        }
+    }
+}
--- cs_calculator/Program.cs
+++ cs_calculator/Program.cs
@@ -6,9 +6,8 @@ namespace cs_calculator
 {
     class Program
     {
-        static void Main (string[] args)
+        public static void CalculateString (string s, bool debug_print = false)
         {
-            string s = "(-3 + 5) * 2 + 3 * 7";
             var queue = StaticParser.Parse (s);
 
             Console.WriteLine (String.Format ("Input: {0}", s));
@@ -19,14 +18,34 @@ namespace cs_calculator
                 return;
             }
 
-            //Console.WriteLine ("\nParsed input:");
-            //foreach (var token in queue)
-            //    Console.WriteLine ($"  {token}");
+            if (debug_print)
+            {
+                Console.WriteLine ("Parsed input:");
+                foreach (Token token in queue)
+                    Console.WriteLine (String.Format ("{0}", token.ToString ()));
+            }
 
             SortingStation.SortAndDie (queue);
-            Console.WriteLine ("\nSorted input:");
-            foreach (var token in queue)
-                Console.WriteLine ($"  {token}");
+
+            if (debug_print)
+            {
+                Console.WriteLine ("Sorted input:");
+                foreach (Token token in queue)
+                    Console.WriteLine (String.Format ("{0}", token.ToString ()));
+            }
+
+            double result = Calculator.CalculateAndDie (queue);
+
+            Console.WriteLine (String.Format ("Result: {0}\n", result));
+        }
+
+        static void Main (string[] args)
+        {
+            CalculateString ("(-3 + 5) * 2 + 3 * 7");
+            CalculateString ("(11 - 12 + 5 * 3 - 2 / 2) / 3", true);
+            CalculateString ("(-3 + 5) * 2 + 3 * 7");
+            CalculateString ("(-3 + 5) * 2 + 3 * 7");
+            CalculateString ("(-3 + 5) * 2 + 3 * 7");
         }
     }
 }
